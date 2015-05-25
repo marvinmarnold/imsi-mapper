bf63d02dcf49b206e22c60bbfa8af03e9e850fec
pili <pili@pili.pili>
2015-05-25 18:02:56 -0500

    Human readable threat level

--- app/helpers/imsi_data_helper.rb
+++ app/helpers/imsi_data_helper.rb
@@ -1,2 +1,11 @@
 module ImsiDataHelper
+	def human_threat_level(lvl_i)
+		{
+			"-1" => "Unknown",
+			"0" => "Idle",
+			"5" => "Normal",
+			"10" => "Medium",
+			"15" => "Alarm"
+		}[lvl_i.to_s]
+	end
 end
