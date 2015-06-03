e86a16cf2d7669b52df5c93da7da882a15df48f1
pili <pili@pili.pili>
2015-06-03 15:59:47 -0500

    Add presence validators

--- app/models/imsi_datum.rb
+++ app/models/imsi_datum.rb
@@ -1,2 +1,4 @@
 class ImsiDatum < ActiveRecord::Base
+	validates :observed_at, :longitude_degrees, :latitude_degrees, :aimsicd_threat_level,
+		presence: true
 end
--- app/models/wifi_datum.rb
+++ app/models/wifi_datum.rb
@@ -1,2 +1,4 @@
 class WifiDatum < ActiveRecord::Base
+	validates :observed_at, :longitude_degrees, :latitude_degrees, :num_wifi_hotspots,
+		presence: true
 end
