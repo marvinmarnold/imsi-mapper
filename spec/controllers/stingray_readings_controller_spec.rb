48b9dfad25296b1d909f0b5da1178b4909d401f9
Crumbie <gapiphafri@throam.com>
2015-08-13 20:58:49 +0000

    working on filling out rspec test for stingray_readings controller

--- spec/controllers/stingray_readings_controller_spec.rb
+++ spec/controllers/stingray_readings_controller_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe StingrayReadingsController, type: :controller do
+    context "with 0 readings" do
+        describe "GET index" do
+            it "has a 200 status code" do
+              get :index
+              expect(response.status).to eq(200)
+            end
+        end
     
+        describe "index" do
+            it "renders the index template" do
+              get :index
+              expect(response.body).to eq "[]"
+            end
+        end
+    end 
 
 end
